ed688f77d93db8c045c68719e110acec9992f700
Abinhav suthan.k.s <abinhavabinhav@gmail.com>
2026-07-14 13:59:40 +0530

    Create 2231-FindFirstPalindromicStringInTheArray

---
+++ 2231-FindFirstPalindromicStringInTheArray/2231-FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,25 @@
+// Last updated: 14/07/2026, 13:59:34
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for(int i=0;i<words.length;i++){
+            if(palin(words[i])){
+                return words[i];
+            }
+        }
+        return "";
+    }
+    public boolean palin(String s){
+        int l=0;
+        int r=s.length()-1;
+        while(l<r){
+            if(s.charAt(l)==s.charAt(r)){
+                l++;
+                r--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}

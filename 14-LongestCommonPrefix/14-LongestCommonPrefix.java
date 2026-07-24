40c34c0a47f3c8b5157936c66c560c319706b303
Abinhav suthan.k.s <abinhavabinhav@gmail.com>
2026-07-24 13:42:16 +0530

    File updated at 24/07/2026, 13:42:16

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.java
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -1,16 +1,16 @@
-// Last updated: 14/07/2026, 14:03:28
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-
-        String pre=strs[0];
-        for(int i=1;i<strs.length;i++){
-            while(!strs[i].startsWith(pre)){
-                pre=pre.substring(0,pre.length()-1);
-            }
-            if(pre.isEmpty()){
-                return "";
-            }
-        }
-        return pre;
-    }
-}
+// Last updated: 24/07/2026, 13:42:16
+1class Solution {
+2    public String longestCommonPrefix(String[] strs) {
+3
+4        String pre=strs[0];
+5        for(int i=1;i<strs.length;i++){
+6            while(!strs[i].startsWith(pre)){
+7                pre=pre.substring(0,pre.length()-1);
+8            }
+9            if(pre==""){
+10                return "";
+11            }
+12        }
+13        return pre;
+14    }
+15}

bff4cff30659595cddb5e0f58b043c83b2065e39
Abinhav suthan.k.s <abinhavabinhav@gmail.com>
2026-07-24 13:51:14 +0530

    File updated at 24/07/2026, 13:51:14

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,13 +1,13 @@
-// Last updated: 14/07/2026, 14:03:22
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            
-            if(nums[i]!=val){
-             nums[c++]=nums[i];
-            }
-        }
-        return c;
-    }
-}
+// Last updated: 24/07/2026, 13:51:14
+1class Solution {
+2    public int removeElement(int[] nums, int val) {
+3        int c=0;
+4        for(int i=0;i<nums.length;i++){
+5            if(nums[i]!=val){
+6                nums[c++]=nums[i];
+7            }
+8        }
+9       
+10        return c;
+11    }
+12}

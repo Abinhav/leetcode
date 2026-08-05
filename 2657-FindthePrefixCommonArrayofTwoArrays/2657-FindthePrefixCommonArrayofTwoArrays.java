5bec4c4ed68876949ee7a8256301b4ce6c86f40b
Abinhav suthan.k.s <abinhavabinhav@gmail.com>
2026-08-05 15:21:26 +0530

    File updated at 05/08/2026, 15:21:25

--- 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.java
+++ 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.java
@@ -1,26 +1,25 @@
-// Last updated: 05/08/2026, 15:00:04
+// Last updated: 05/08/2026, 15:21:25
 1class Solution {
-2    public int[] findThePrefixCommonArray(int[] a, int[] b) {
-3        int c[]=new int[a.length];
-4        int k=0;
-5        if(a[0]==b[0]){
-6            c[k++]=1;
-7        }
-8        else{
-9            c[k++]=0;
-10        }
-11        HashSet<Integer> set=new HashSet<>();
-12        set.add(a[0]);
-13        for(int i=1;i<b.length;i++){
-14            set.add(a[i]);
-15            int f=0;
-16            for(int j=0;j<=i;j++){
-17             if(set.contains(b[j])){
-18                f++;
-19             }
-20            }
-21            c[k++]=f;
-22        }
-23        return c;
-24    }
-25}
+2    public int longestMonotonicSubarray(int[] nums) {
+3        int ans=1;
+4        int in=1,dc=1;
+5        for(int i=0;i<nums.length-1;i++){
+6            if(nums[i]<nums[i+1]){
+7                   in++;
+8            }
+9            else{
+10                in=1;
+11            }
+12            if(nums[i]>nums[i+1]){
+13                dc++;
+14            }
+15            else{
+16                dc=1;
+17            }
+18            ans=Math.max(ans,Math.max(in,dc));
+19        }
+20        
+21        return ans;
+22        
+23    }
+24}

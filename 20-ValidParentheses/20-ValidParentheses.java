ee3c5f4123c0161b2d1490ab31b0cfb7bebe8859
Abinhav suthan.k.s <abinhavabinhav@gmail.com>
2026-08-18 12:03:37 +0530

    File updated at 18/08/2026, 12:03:29

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,21 +1,17 @@
-// Last updated: 14/07/2026, 14:03:26
-class Solution {
-    public boolean isValid(String a) {
-       Stack<Character> s=new Stack<>();
-       for(char x:a.toCharArray()){
-        if(x=='('||x=='['||x=='{'){
-            s.push(x);
-        }
-        else{
-            if(s.isEmpty()){
-                return false;
-            }
-            char top=s.pop();
-            if(x==')'&&top!='('||x==']'&&top!='['||x=='}'&&top!='{'){
-                return false;
-            }
-        }
-       }
-       return s.isEmpty();
-    }
-}
+// Last updated: 18/08/2026, 12:03:29
+1class Solution {
+2    public boolean isValid(String a) {
+3      Stack<Character> st=new Stack<>();
+4      for(char x:a.toCharArray()){
+5        if(x=='['||x=='('||x=='{'){
+6            st.push(x);
+7        }
+8        else{
+9            if(st.isEmpty()) return false;
+10            char top=st.pop();
+11            if(x==')'&&top!='('||x=='}'&&top!='{'||x==']'&&top!='[') return false;
+12        }
+13      }
+14      return st.isEmpty();
+15    }
+16}

146448197ed87c90183080a0602f7868485fdfbb
Abinhav suthan.k.s <abinhavabinhav@gmail.com>
2026-08-22 18:46:27 +0530

    File updated at 22/08/2026, 18:46:23

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,20 +1,23 @@
-// Last updated: 14/07/2026, 14:02:08
-class Solution {
-    public boolean isAnagram(String s, String t) {
-       if(s.length()!=t.length())        {
-        return false;
-       }
-       char[] a=s.toCharArray();
-       char[] b=t.toCharArray();
-       Arrays.sort(a);
-       Arrays.sort(b);
-       boolean bool=true;
-       for(int i=0;i<s.length();i++){
-        if(a[i]!=b[i]){
-          
-            return false;
-        }
-       }
-       return true;
-    }
-}
+// Last updated: 22/08/2026, 18:46:23
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3       if(s.length()!=t.length())        {
+4        return false;
+5       }
+6       HashMap<Character,Integer> map=new HashMap<>();
+7       for(char x: s.toCharArray()){
+8        map.put(x,map.getOrDefault(x,0)+1);
+9       }
+10       for(char x:t.toCharArray()){
+11        if(!map.containsKey(x)){
+12            return false;
+13            
+14        }
+15        map.put(x,map.get(x)-1);
+16        if(map.get(x)==0){
+17            map.remove(x);
+18        }
+19       }
+20       return map.isEmpty();
+21    }
+22}

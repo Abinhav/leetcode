f0eea6db440ff2e389cb1396a54cfb46c819f011
Abinhav suthan.k.s <abinhavabinhav@gmail.com>
2026-08-31 14:51:41 +0530

    File updated at 31/08/2026, 14:51:37

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,17 +1,17 @@
-// Last updated: 14/07/2026, 14:01:17
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    c++;
-                }
-
-            }
-        }
-        return c;
-    }
-}
+// Last updated: 31/08/2026, 14:51:37
+1class Solution {
+2    public int subarraySum(int[] nums, int k) {
+3        int c=0;
+4        for(int i=0;i<nums.length;i++){
+5            int sum=0;
+6            for(int j=i;j<nums.length;j++){
+7                sum+=nums[j];
+8                if(sum==k){
+9                    c++;
+10                }
+11
+12            }
+13        }
+14        return c;
+15    }
+16}

059c42ddcf850a5c93781334094662ac2b5e1a3a
Abinhav suthan.k.s <abinhavabinhav@gmail.com>
2026-09-02 16:08:39 +0530

    Create

---
+++ problem-1788345510694/problem-1788345510694.java
@@ -0,0 +1,13 @@
+// Last updated: 02/09/2026, 16:08:30
+1class Solution {
+2    public int trailingZeroes(int n) {
+3        int ans = Math.floorDiv(n, 5);
+4        ans += Math.floorDiv(n, 25);
+5        ans += Math.floorDiv(n, 125);
+6        ans += Math.floorDiv(n, 625);
+7        ans += Math.floorDiv(n, 3125);
+8        
+9        
+10        return ans;
+11    }
+12}
